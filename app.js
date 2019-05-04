ba1175b010f76f8a0f4a2babdfe3d4fe4f77f0ef
prshntattri1 <33720370+prshntattri1@users.noreply.github.com>
2019-05-04 23:00:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var os = require('os');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  text = "Running Node.js:" + process.versions.node + "\n" + os.hostname() + "\nIt Works! Hello\n" + "\nCICD Successful!\n";
+  text = "Running Node.js:" + process.versions.node + "\n" + os.hostname() + "\nIt Works! Hello\n" + "\nCICD Successful!!\n";
   console.log("Received Message From:" + JSON.stringify(request.headers ));
   response.end(text);
 });
